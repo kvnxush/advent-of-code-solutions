03a2be1712b1bbda3adfaa71b2679d89661cae8c
Kevin Ushijima <kushijima@netflix.com>
2022-12-05 10:17:18 -0800

    remove submodule

--- 2022/utils.js
+++ 2022/utils.js
@@ -16,19 +16,3 @@ module.exports = {
     return parsedInput.map(parseAs);
   },
 };
-
-/* new day template
-const { answer, parseInput } = require('../utils');
-const input = parseInput('day1', { parseAs: Number });
-
-function answer1(values) {
-}
-
-function answer2(values) {
-}
-
-answer([
-  ['day1/part1', answer1(input)],
-  ['day1/part2', answer2(input)],
-])
-*/
