c3e1df6bdfef365edb691040c8c2ee015227218e
Kevin Ushijima <kushijima@netflix.com>
2023-11-09 11:21:59 -0800

    2023 setup

---
+++ 2023/day1/solution.js
@@ -0,0 +1,11 @@
+const { answer, parseInput } = require('../utils');
+const input = parseInput('day1', { parseAs: Number });
+
+function answer1() {}
+
+function answer2() {}
+
+answer([
+  ['day1/part1', answer1(input)],
+  ['day1/part2', answer2(input)],
+]);
